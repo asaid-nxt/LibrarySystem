f94fefc898d696c4df33bba734cd327ae84d447b
asaid-nxt <asaid@nxt.so>
2024-08-22 14:25:31 +0300

    added test cases for books genre filtering

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -3,19 +3,24 @@ require "test_helper"
 class BooksControllerTest < ActionDispatch::IntegrationTest
   setup do
     Book.delete_all
-    @book1 = Book.create!(title: "Book 1", author: "Author 1", genre: "Fiction", isbn: "1234567890", available_copies: 5)
-    @book2 = Book.create!(title: "Book 2", author: "Author 2", genre: "Non-Fiction", isbn: "0987654321", available_copies: 3)
+    @fiction_book = Book.create!(title: "Fiction Book", author: "Author 1", genre: "Fiction", isbn: "1234567890", available_copies: 5)
+    @non_fiction_book = Book.create!(title: "Non-Fiction Book", author: "Author 2", genre: "Non-Fiction", isbn: "0987654321", available_copies: 3)
   end
 
-  test "should get index" do
+  test "should get all books" do
     get books_url, as: :json
     assert_response :success
 
     json_response = JSON.parse(response.body)
-
     assert_equal 2, json_response.length
+  end
 
-    assert_equal @book1.title, json_response[0]["title"]
-    assert_equal @book2.title, json_response[1]["title"]
+  test "should get books filtered by genre" do
+    get books_url(genre: "Fiction"), as: :json
+    assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_equal 1, json_response.length
+    assert_equal @fiction_book.title, json_response[0]["title"]
   end
 end
