a8526e285cc2c5969e2c4411fe19f380fd3ea151
asaid-nxt <asaid@nxt.so>
2024-08-22 16:06:43 +0300

    Added validation for Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
+  validates :title, :author, :genre, :isbn, :available_copies, presence: true
   scope :by_genre, ->(genre) {where("genre ILIKE ?", genre) if genre.present?}
 end
