b8b1e3678f240ac65251cad4d9a24139199dc303
asaid-nxt <asaid@nxt.so>
2024-08-23 21:00:21 +0300

    Added devise user authentication full docs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,115 +1,288 @@
+# spec/integration/user_authentication_spec.rb
 require 'swagger_helper'
 
-RSpec.describe 'Users API', type: :request do
+RSpec.describe 'User Authentication', type: :request do
 
-  # User sign-up route
-  path '/users' do
-    post 'Signs up a user' do
-      tags 'Users'
-      consumes 'application/json'
-      produces 'application/json'
-
-      parameter name: :user, in: :body, schema: {
-        type: :object,
-        properties: {
-          user: {
-            type: :object,
-            properties: {
-              email: { type: :string, example: 'newuser@example.com' },
-              password: { type: :string, example: 'password123' }
-            },
-            required: ['email', 'password', 'password_confirmation']
-          }
-        },
-        required: ['user']
-      }
-
-      response '201', 'user created' do
-        let(:user) { { user: { email: 'newuser@example.com', password: 'password123'} } }
-        skip 'Skipping test execution, only generating docs'
-      end
-
-      response '422', 'invalid request' do
-        let(:user) { { user: { email: 'newuser@example.com', password: 'password123'} } }
-        skip 'Skipping test execution, only generating docs'
-      end
-    end
+  before do
+    # Stubbing the requests to skip actual execution
+    allow_any_instance_of(ActionDispatch::Integration::Session).to receive(:post).and_return(true)
+    allow_any_instance_of(ActionDispatch::Integration::Session).to receive(:delete).and_return(true)
+    allow_any_instance_of(ActionDispatch::Integration::Session).to receive(:patch).and_return(true)
+    allow_any_instance_of(ActionDispatch::Integration::Session).to receive(:put).and_return(true)
   end
 
-  # User sign-in route
   path '/users/sign_in' do
-    post 'Signs in a user' do
-      tags 'Users'
-      consumes 'application/json'
+    get 'User Sign in page' do
+      tags 'User Authentication'
       produces 'application/json'
 
+      response '200', 'sign in page' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+
+    post 'User Sign in' do
+      tags 'User Authentication'
+      consumes 'application/json'
+      produces 'application/json'
       parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
-          user: {
-            type: :object,
-            properties: {
-              email: { type: :string, example: 'user@example.com' },
-              password: { type: :string, example: 'password123' }
-            },
-            required: ['email', 'password']
-          }
+          email: { type: :string, example: 'user@example.com' },
+          password: { type: :string, example: 'password' }
         },
-        required: ['user']
+        required: ['email', 'password']
       }
 
-      response '200', 'user signed in' do
-        let!(:user) { User.create!(email: 'test@test.com', password: '123456') }
-        let(:user) { { user: { email: 'test@test.com', password: '123456' } } }
-        skip 'Skipping test execution, only generating docs'
+      response '200', 'successful sign in' do
+        let(:user) { { email: 'user@example.com', password: 'password' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
       end
 
       response '401', 'unauthorized' do
-        let(:user) { { user: { email: 'wrong@test.com', password: 'wrongpassword' } } }
-        skip 'Skipping test execution, only generating docs'
+        let(:user) { { email: 'wrong@example.com', password: 'wrong_password' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
       end
     end
   end
 
-  # User sign-out route
   path '/users/sign_out' do
-    delete 'Signs out a user' do
-      tags 'Users'
+    delete 'User Sign out' do
+      tags 'User Authentication'
+      produces 'application/json'
+
+      response '204', 'successful sign out' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+  end
+
+  path '/users/password/new' do
+    get 'New password reset page' do
+      tags 'User Authentication'
+      produces 'application/json'
+
+      response '200', 'password reset page' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+  end
+
+  path '/users/password/edit' do
+    get 'Edit password page' do
+      tags 'User Authentication'
+      produces 'application/json'
+
+      response '200', 'edit password page' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+  end
+
+  path '/users/password' do
+    post 'Send reset password instructions' do
+      tags 'User Authentication'
       consumes 'application/json'
       produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string, example: 'user@example.com' }
+        },
+        required: ['email']
+      }
 
-      response '204', 'user signed out' do
-        let!(:user) { User.create!(email: 'test@test.com', password: '123456') }
-
-        before do
-          sign_in(user)
+      response '200', 'password reset email sent' do
+        let(:user) { { email: 'user@example.com' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
         end
+      end
 
-        skip 'Skipping test execution, only generating docs'
+      response '404', 'email not found' do
+        let(:user) { { email: 'nonexistent@example.com' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+
+    patch 'Update password' do
+      tags 'User Authentication'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          reset_password_token: { type: :string, example: 'reset-token' },
+          password: { type: :string, example: 'newpassword' },
+          password_confirmation: { type: :string, example: 'newpassword' }
+        },
+        required: ['reset_password_token', 'password', 'password_confirmation']
+      }
+
+      response '200', 'password updated' do
+        let(:user) { { reset_password_token: 'reset-token', password: 'newpassword', password_confirmation: 'newpassword' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+
+      response '422', 'unprocessable entity' do
+        let(:user) { { reset_password_token: 'reset-token', password: 'short', password_confirmation: 'mismatch' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+
+    put 'Update password' do
+      tags 'User Authentication'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          reset_password_token: { type: :string, example: 'reset-token' },
+          password: { type: :string, example: 'newpassword' },
+          password_confirmation: { type: :string, example: 'newpassword' }
+        },
+        required: ['reset_password_token', 'password', 'password_confirmation']
+      }
+
+      response '200', 'password updated' do
+        let(:user) { { reset_password_token: 'reset-token', password: 'newpassword', password_confirmation: 'newpassword' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+
+      response '422', 'unprocessable entity' do
+        let(:user) { { reset_password_token: 'reset-token', password: 'short', password_confirmation: 'mismatch' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
       end
     end
   end
 
-  # Current user details route
-  path '/users/me' do
-    get 'Retrieves current user details' do
-      tags 'Users'
+  path '/users/sign_up' do
+    get 'New registration page' do
+      tags 'User Authentication'
       produces 'application/json'
 
-      response '200', 'user details' do
-        let!(:user) { User.create!(email: 'test@test.com', password: '123456') }
-
-        before do
-          sign_in(user)
+      response '200', 'registration page' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
         end
+      end
+    end
 
-        skip 'Skipping test execution, only generating docs'
+    post 'Create user' do
+      tags 'User Authentication'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string, example: 'newuser@example.com' },
+          password: { type: :string, example: 'password' },
+          password_confirmation: { type: :string, example: 'password' }
+        },
+        required: ['email', 'password', 'password_confirmation']
+      }
+
+      response '201', 'successful registration' do
+        let(:user) { { email: 'newuser@example.com', password: 'password', password_confirmation: 'password' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
       end
 
-      response '401', 'unauthorized' do
-        skip 'Skipping test execution, only generating docs'
+      response '422', 'unprocessable entity' do
+        let(:user) { { email: 'invalidemail', password: 'short', password_confirmation: 'different' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
       end
     end
   end
 
+  path '/users/cancel' do
+    get 'Cancel registration page' do
+      tags 'User Authentication'
+      produces 'application/json'
+
+      response '200', 'cancel registration page' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+  end
+
+  path '/users/edit' do
+    get 'Edit registration page' do
+      tags 'User Authentication'
+      produces 'application/json'
+
+      response '200', 'edit registration page' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+
+    patch 'Update user' do
+      tags 'User Authentication'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string, example: 'updateduser@example.com' },
+          password: { type: :string, example: 'newpassword' },
+          password_confirmation: { type: :string, example: 'newpassword' }
+        },
+        required: ['email', 'password', 'password_confirmation']
+      }
+
+      response '200', 'user updated' do
+        let(:user) { { email: 'updateduser@example.com', password: 'newpassword', password_confirmation: 'newpassword' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+
+      response '422', 'unprocessable entity' do
+        let(:user) { { email: 'invalidemail', password: 'short', password_confirmation: 'mismatch' } }
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+
+    delete 'Delete user' do
+      tags 'User Authentication'
+      produces 'application/json'
+
+      response '204', 'user deleted' do
+        skip 'Skipping test execution, but generating docs' do
+          run_test!
+        end
+      end
+    end
+  end
 end
