9fa7c88e64ea3ca7c3d874e0a7cb094084ff915a
asaid-nxt <asaid@nxt.so>
2024-08-26 13:30:47 +0300

    Generated resources for borrowing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_134346) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_26_102742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,18 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_134346) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "borrowings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "borrowed_at", null: false
+    t.datetime "due_date", null: false
+    t.datetime "returned_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_borrowings_on_book_id"
+    t.index ["user_id"], name: "index_borrowings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -48,4 +60,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_134346) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "borrowings", "books"
+  add_foreign_key "borrowings", "users"
 end
