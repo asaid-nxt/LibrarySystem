80db17d0a0f0139c1e2b63464710d8beff751d71
asaid-nxt <asaid@nxt.so>
2024-08-27 13:45:23 +0300

    Fixed routes for borrowings controller

--- config/routes.rb
+++ config/routes.rb
@@ -16,16 +16,19 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
+
   resources :books do
     resources :borrowings, only: [:create] do
-      collection do
-        get :overdue
-      end
-      member do
-        patch :return
-      end
+      patch :return, on: :collection
     end
   end
+
+  resources :borrowings, only: [] do
+    collection do
+      get :overdue
+    end
+  end
+
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
