3affb5ca02b9f889b59cfa62ac4fb5dbdeae1c77
asaid-nxt <asaid@nxt.so>
2024-08-28 16:14:46 +0300

    Added unit tests for admin model

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,26 @@
-# require 'rails_helper'
+# spec/models/admin_spec.rb
+require 'rails_helper'
 
-# RSpec.describe Admin, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Admin, type: :model do
+  let(:admin) { build(:admin) }
+
+  it 'is valid with valid attributes' do
+    expect(admin).to be_valid
+  end
+
+  it 'is not valid without an email' do
+    admin.email = nil
+    expect(admin).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    admin.password = nil
+    expect(admin).to_not be_valid
+  end
+
+  it 'does not allow duplicate emails' do
+    create(:admin, email: 'admin@example.com')
+    duplicate_admin = build(:admin, email: 'admin@example.com')
+    expect(duplicate_admin).to_not be_valid
+  end
+end
